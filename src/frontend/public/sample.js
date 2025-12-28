10b7309d02120f8729c147e10860283a48147d1c
yamrisa <riffiy.6135@gmail.com>
2025-12-28 22:56:27 +0900

    TypeScriptをブラウザ表示できる環境を構築

--- src/frontend/public/sample.js
+++ src/frontend/public/sample.js
@@ -3,18 +3,16 @@
 // function logMessage(message: string): void {
 //     console.log("logMessage:", message);
 // }
-  
 // // 値を返す関数
 //     function double(n: number): number {
 //     return n * 2;
 // }
-
 // // 実行
 // logMessage("Hello TypeScript");
-
 // const result = double(5);
 // console.log("double result:", result);
-
 // ブラウザ上で確認
-const message: string = "Hello TypeScript!";
+const message = "Hello TypeScript!";
 console.log(message);
+export {};
+//# sourceMappingURL=sample.js.map
--- src/frontend/src/sample.js
+++ src/frontend/src/sample.js
@@ -1,11 +1,15 @@
-// function double (n:number){
-//     return n * 2
-// }
-// const result = double(5)
-// console.log(result)
-function log(message) {
-    console.log(message);
-    return message;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// 値を返さない（void）関数
+function logMessage(message) {
+    console.log("logMessage:", message);
 }
-var result = log("hello");
-console.log(result);
+// 値を返す関数
+function double(n) {
+    return n * 2;
+}
+// 実行
+logMessage("Hello TypeScript");
+const result = double(5);
+console.log("double result:", result);
+//# sourceMappingURL=sample.js.map
